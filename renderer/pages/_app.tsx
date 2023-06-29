aadaa9c255fec4a596465e72dab04317c3ad3e9d
Dev Rabbit <131601029+bravedevrabbit@users.noreply.github.com>
2023-06-28 23:26:11 -0400

    Update _app.tsx
    update

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 }
 
